865fbe0d0196419fc9a6406e8271c768382efea6
Goddy01 <adigungodwin2@gmail.com>
2023-07-14 19:53:05 +0100

    reg'd the Message model its admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin 
+from . import models
 # Register your models here.
+
+class MessagesAdmin(ImportExportModelAdmin):
+    list_display = ['user', 'sender', 'recipient', 'date', 'is_read']
+    list_filter = ['is_read']
+
+admin.site.register(models.Message, MessagesAdmin)
