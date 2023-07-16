66e9b25652ec3e5e00a132aaeebd512afdb32ddf
Goddy01 <adigungodwin2@gmail.com>
2023-07-16 14:32:46 +0100

    reg'd UserProfile in accounts.admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import UserProfile
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
+
+class UserProfileAdmin(ImportExportModelAdmin):
+    list_display = ['user', 'full_name', 'email', 'bio']
+
+admin.site.register(UserProfile, UserProfileAdmin)
