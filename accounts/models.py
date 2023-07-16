eea69e97c3c66c065c277c141aa12af984ef4cfa
Goddy01 <adigungodwin2@gmail.com>
2023-07-16 20:10:36 +0100

    added save_user_profile method in accounts.models

--- accounts/models.py
+++ accounts/models.py
@@ -39,3 +39,6 @@ class UserProfile(AbstractBaseUser):
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             UserProfile.objects.create(user=instance)
+
+    def save_user_profile(sender, instance, created, **kwargs):
+        instance.profile.save()
