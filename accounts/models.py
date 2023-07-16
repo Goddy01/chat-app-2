718d4789ddc5baf8624652edbd4faf81851eabbb
Goddy01 <adigungodwin2@gmail.com>
2023-07-16 13:50:58 +0100

    overwrote the default save method

--- accounts/models.py
+++ accounts/models.py
@@ -18,3 +18,12 @@ class Profile(User):
 
     def __str__(self):
         return f"{self.user.username}"
+    
+    def save(self, *args, **kwargs):
+        """Overwrites the base save method"""
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300) # height, width
+            img.thumbnail(output_size)
+            img.save(self.image.path)
