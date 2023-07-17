91dcf15cdf10eace050110f81d70fafa18c5f40e
Goddy01 <adigungodwin2@gmail.com>
2023-07-17 10:43:16 +0100

    added send_direct url to core.urls

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.home_chat, name='index'),
     path('directs/<str:username>/', views.direct_messages, name='direct_messages'),
+    path('send/', views.send_direct, name='send-direct'),
 ]
